08e663f77cef8c971d6fb840dc99fcb8d8b44598
hanifan <hanifannurulhaq@outlook.com>
2023-06-23 21:10:57 +0700

    add custom text widget

---
+++ lib/widgets/text_widget.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class CustomTextWidget extends StatelessWidget {
+  const CustomTextWidget({
+    required this.text, 
+    super.key,
+    this.size = 12, 
+    this.weight = FontWeight.w400, 
+    this.color = Colors.black,
+    this.textAlign = TextAlign.start,
+    this.isUsedMaxLines = true,
+  });
+
+  final String text;
+  final double size;
+  final FontWeight weight;
+  final Color color;
+  final TextAlign textAlign;
+  final bool isUsedMaxLines;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      maxLines: isUsedMaxLines ? 3 : null,
+      textAlign: textAlign,
+      style: TextStyle(
+        fontSize: size,
+        fontWeight: weight,
+        color: color,
+        overflow: isUsedMaxLines ? TextOverflow.ellipsis : TextOverflow.visible,
+      ),
+    );
+  }
+}
