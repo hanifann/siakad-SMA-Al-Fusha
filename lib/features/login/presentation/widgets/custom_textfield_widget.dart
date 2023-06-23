afd8564e4cc47bc538f7991c0730eae076bacfce
hanifan <hanifannurulhaq@outlook.com>
2023-06-23 21:10:37 +0700

    add custom textfield widget

---
+++ lib/features/login/presentation/widgets/custom_textfield_widget.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:siakad_sma_al_fusha/themes/colors.dart';
+
+class CustomTextfieldWidget extends StatelessWidget {
+  const CustomTextfieldWidget({
+    required this.controller, 
+    required this.hint,
+    this.suffixIcon,
+    this.isObscure = false,
+    this.onSuffixTap,
+    super.key,
+  });
+  final TextEditingController controller;
+  final String hint;
+  final IconData? suffixIcon;
+  final bool isObscure;
+  final VoidCallback? onSuffixTap;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller,
+      cursorColor: kPrimaryColor,
+      obscureText: isObscure,
+      style: TextStyle(
+        fontWeight: FontWeight.w500,
+        fontSize: 16.sp,
+      ),
+      decoration: InputDecoration(
+        isDense: true,
+        hintText: hint,
+        suffixIcon: GestureDetector(
+          onTap: onSuffixTap,
+          child: Icon(
+            suffixIcon,
+            size: 24.w,
+            color: kPrimaryTextColor,
+          ),
+        ),
+        border: InputBorder.none,
+        enabledBorder: UnderlineInputBorder(
+          borderSide: BorderSide(
+            color: controller.text.isEmpty ? kPrimaryTextColor : kPrimaryColor,
+            width: 2,
+          ),
+        ),
+        focusedBorder: const UnderlineInputBorder(
+          borderSide: BorderSide(
+            color: kPrimaryColor,
+            width: 2,
+          ),
+        ),
+      ),
+    );
+  }
+}
