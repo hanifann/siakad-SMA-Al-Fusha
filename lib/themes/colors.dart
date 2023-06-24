9c2d8fd53ef367ec187387c1009bfc2baa604160
hanifan <hanifannurulhaq@outlook.com>
2023-06-24 22:12:19 +0700

    add new color

--- lib/themes/colors.dart
+++ lib/themes/colors.dart
@@ -3,3 +3,4 @@ import 'package:flutter/material.dart';
 const kPrimaryTextColor = Color.fromRGBO(18, 19, 22, 1);
 const kPrimaryColor = Color.fromRGBO(0, 162, 233, 1);
 const kErrorColor = Color.fromRGBO(200, 0, 3, 1);
+const kSecondaryTextColor = Color.fromRGBO(111, 112, 114, 1);
