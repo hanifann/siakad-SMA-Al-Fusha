320e669948487e4e8b37ab197d18a853963c5e6d
hanifan <hanifannurulhaq@outlook.com>
2023-06-26 21:34:47 +0700

    inject getIt

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,6 +1,11 @@
+
+import 'package:flutter/widgets.dart';
 import 'package:siakad_sma_al_fusha/app/app.dart';
 import 'package:siakad_sma_al_fusha/bootstrap.dart';
+import 'injection_container.dart' as di;
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
   bootstrap(() => const App());
 }
