e2dc6c247ae8bb60badec9e632660696f268cdf1
hanifan <hanifannurulhaq@outlook.com>
2023-06-26 22:18:29 +0700

    inject login dependency

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,7 +1,17 @@
 import 'package:get_it/get_it.dart';
+import 'package:http/http.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:siakad_sma_al_fusha/core/platform/network_info.dart';
 import 'package:siakad_sma_al_fusha/features/auth/presentation/cubit/auth_cubit.dart';
+import 'package:siakad_sma_al_fusha/features/login/data/datasources/login_local_data_source.dart';
+import 'package:siakad_sma_al_fusha/features/login/data/datasources/login_remote_datasource.dart';
+import 'package:siakad_sma_al_fusha/features/login/data/repositories/login_repository_impl.dart';
+import 'package:siakad_sma_al_fusha/features/login/domain/repositories/login_repositoy.dart';
+import 'package:siakad_sma_al_fusha/features/login/domain/usecase/get_user_usecase.dart';
+import 'package:siakad_sma_al_fusha/features/login/domain/usecase/post_login_usecase.dart';
+import 'package:siakad_sma_al_fusha/features/login/presentation/bloc/login_bloc.dart';
+import 'package:siakad_sma_al_fusha/features/login/presentation/bloc/user_bloc.dart';
 
 final sl = GetIt.instance;
 
@@ -13,9 +23,33 @@ Future<void> init() async {
 
   //login
   //bloc
+  sl.registerFactory(() => LoginBloc(sl()));
+  sl.registerFactory(() => UserBloc(sl()));
+  //usecases
+  sl.registerLazySingleton(() => PostLoginUseCase(repository: sl()));
+  sl.registerLazySingleton(() => GetUserUseCase(repository: sl()));
+  //repository
+  sl.registerLazySingleton<LoginRepository>(
+    () => LoginRepositoryImpl(
+      remoteDataSource: sl(), 
+      localDataSource: sl(), 
+      networkInfo: sl()
+    )
+  );
+  //datasources
+  sl.registerLazySingleton<LoginRemoteDataSource>(
+    () => LoginRemoteDataSourceImpl(client: sl(), preferences: sl())
+  );
+  sl.registerLazySingleton<LoginLocalDataSource>(
+    () => LoginLocalDataSourceImpl(preferences: sl())
+  );
+  
 
   //!external
   final prefs = await SharedPreferences.getInstance();
+  final client = Client();
+  sl.registerLazySingleton(() => client);
   sl.registerLazySingleton(() => prefs);
   sl.registerLazySingleton(() => InternetConnectionChecker());
+  sl.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(sl()));
 }
