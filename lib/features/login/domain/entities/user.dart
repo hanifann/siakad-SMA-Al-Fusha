df6217921ce6b18281cacec3e954e8f7200e29a5
hanifan <hanifannurulhaq@outlook.com>
2023-06-26 20:00:18 +0700

    add user entity

---
+++ lib/features/login/domain/entities/user.dart
@@ -0,0 +1,62 @@
+import 'package:equatable/equatable.dart';
+
+class User extends Equatable{
+    final List<UserDatum> data;
+
+    const User({
+        required this.data,
+    });
+    
+      @override
+      List<Object?> get props => [
+        data
+      ];
+
+}
+
+class UserDatum extends Equatable{
+    final String id;
+    final String nama;
+    final String username;
+    final String role;
+    final String idUsers;
+    final String? tempat;
+    final DateTime? tglLahir;
+    final String? pendidikan;
+    final DateTime? tmk;
+    final String? jabatan;
+    final String alamat;
+    final String? tgsTam;
+
+    const UserDatum({
+        required this.id,
+        required this.nama,
+        required this.username,
+        required this.role,
+        required this.idUsers,
+        required this.tempat,
+        required this.tglLahir,
+        required this.pendidikan,
+        required this.tmk,
+        required this.jabatan,
+        required this.alamat,
+        required this.tgsTam,
+    });
+    
+      @override
+      List<Object?> get props => [
+        id,
+        nama,
+        username,
+        role,
+        idUsers,
+        tempat,
+        tglLahir,
+        pendidikan,
+        tmk,
+        jabatan,
+        alamat,
+        tgsTam
+      ];
+
+}
