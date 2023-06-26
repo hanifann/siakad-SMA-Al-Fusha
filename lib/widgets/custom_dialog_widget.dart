944cdb19002ea7700cacaa59b2e675ac7187c251
hanifan <hanifannurulhaq@outlook.com>
2023-06-26 22:19:17 +0700

    add loading dialog, edit padding on error dialog

--- lib/widgets/custom_dialog_widget.dart
+++ lib/widgets/custom_dialog_widget.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:siakad_sma_al_fusha/themes/colors.dart';
 
 class ErrorDialog extends StatelessWidget {
   const ErrorDialog({required this.errorValue, super.key, });
@@ -9,12 +13,22 @@ class ErrorDialog extends StatelessWidget {
   Widget build(BuildContext context) {
     return AlertDialog(
       title: Text(
-        errorValue,
-        style: const TextStyle(
-          fontWeight: FontWeight.w500,
-          fontSize: 14,
+        'Error',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 18.sp,
         ),
       ),
+      titlePadding: EdgeInsets.symmetric(horizontal: 8.w, vertical: 8.h),
+      contentPadding: EdgeInsets.symmetric(horizontal: 8.w),
+      content: Text(
+        errorValue,
+        style: TextStyle(
+          fontWeight: FontWeight.w500,
+          fontSize: 14.sp,
+        ),
+      ),
+      actionsPadding: EdgeInsets.symmetric(horizontal: 8.w, vertical: 8.h),
       actions: [
         TextButton(
           onPressed: ()=> Navigator.pop(context), 
@@ -25,28 +39,29 @@ class ErrorDialog extends StatelessWidget {
   }
 }
 
-// class LoadingDialog extends StatelessWidget {
-//   const LoadingDialog({super.key});
+class LoadingDialog extends StatelessWidget {
+  const LoadingDialog({super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return AlertDialog(
-//       title: Column(
-//         children: [
-//           CircularProgressIndicator(
-//             valueColor: AlwaysStoppedAnimation(kPrimaryColor),
-//             backgroundColor: Colors.white,
-//           ),
-//           const SizedBox(height: 16),
-//           const Text(
-//             'Loading . . .',
-//             style: TextStyle(
-//               fontWeight: FontWeight.w500,
-//               fontSize: 14,
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Column(
+        children: [
+          CircularProgressIndicator.adaptive(
+            valueColor: const AlwaysStoppedAnimation(kPrimaryColor),
+            backgroundColor: 
+              Platform.isAndroid ? Colors.white : kPrimaryColor,
+          ),
+          const SizedBox(height: 16),
+          const Text(
+            'Memuat . . .',
+            style: TextStyle(
+              fontWeight: FontWeight.w500,
+              fontSize: 14,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
