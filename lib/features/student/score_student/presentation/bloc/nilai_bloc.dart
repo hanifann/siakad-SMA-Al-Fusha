c94f7376e2cd224fac6161d87757634edb53e6af
hanifan <hanifannurulhaq@outlook.com>
2023-06-27 19:28:21 +0700

    add getUserId function

--- lib/features/student/score_student/presentation/bloc/nilai_bloc.dart
+++ lib/features/student/score_student/presentation/bloc/nilai_bloc.dart
@@ -1,6 +1,9 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:siakad_sma_al_fusha/core/error/model/error.dart';
+import 'package:siakad_sma_al_fusha/core/usecase/usecase.dart';
+import 'package:siakad_sma_al_fusha/features/login/domain/entities/user.dart';
+import 'package:siakad_sma_al_fusha/features/student/home_student/domain/usecases/get_role_usecase.dart';
 import 'package:siakad_sma_al_fusha/features/student/score_student/domain/entities/nilai.dart';
 import 'package:siakad_sma_al_fusha/features/student/score_student/domain/usecases/get_nilai_usecase.dart';
 
@@ -8,11 +11,13 @@ part 'nilai_event.dart';
 part 'nilai_state.dart';
 
 class NilaiBloc extends Bloc<NilaiEvent, NilaiState> {
-  NilaiBloc(this.useCase) : super(NilaiInitial()) {
+  NilaiBloc(this.useCase, this.roleUseCase) : super(NilaiInitial()) {
     on<GetNilaiEvent>(_onGetNilaiEvent);
+    on<GetUserIdEvent>(_onGetUSerIdEvent);
   }
 
   final GetNilasiUseCase useCase;
+  final GetRoleUseCase roleUseCase;
 
   void _onGetNilaiEvent(
     GetNilaiEvent event,
@@ -23,6 +28,18 @@ class NilaiBloc extends Bloc<NilaiEvent, NilaiState> {
     result!.fold(
       (l) => emit(NilaiFailed(ErrorModel(message: l.message))), 
       (r) => emit(NilaiLoaded(r))
+    );
+  }
+
+  void _onGetUSerIdEvent(
+    GetUserIdEvent event,
+    Emitter<NilaiState> emit,
+  ) async {
+    emit(NilaiLoading());
+    final userId = await roleUseCase(NoParams());
+    userId!.fold(
+      (l) => emit(const NilaiFailed(ErrorModel())),
+      (r) => emit(UserDataLoaded(r))
     );
   }
 
