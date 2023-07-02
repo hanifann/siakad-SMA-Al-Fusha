5c289744845fa99ea08eb8329bad26a6257280d4
hanifan <hanifannurulhaq@outlook.com>
2023-07-02 17:36:16 +0700

    extends equatable

--- lib/features/student/announcement_student/domain/entities/announcement.dart
+++ lib/features/student/announcement_student/domain/entities/announcement.dart
@@ -1,23 +1,36 @@
-class Announcement {
+import 'package:equatable/equatable.dart';
+
+class Announcement extends Equatable{
     final List<AnnouncementDatum> data;
 
-    Announcement({
+    const Announcement({
         required this.data,
     });
+    
+      @override
+      List<Object?> get props => [data];
 
 }
 
-class AnnouncementDatum {
+class AnnouncementDatum extends Equatable{
     final String id;
     final DateTime tanggal;
     final String judul;
     final String isiPengumuman;
 
-    AnnouncementDatum({
+    const AnnouncementDatum({
         required this.id,
         required this.tanggal,
         required this.judul,
         required this.isiPengumuman,
     });
+    
+      @override
+      List<Object?> get props => [
+        id,
+        tanggal,
+        judul,
+        isiPengumuman
+      ];
 
 }
