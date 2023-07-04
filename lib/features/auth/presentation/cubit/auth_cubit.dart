035bb6a57bc631da6f6aa68d156e4b83f14f349e
hanifan <hanifannurulhaq@outlook.com>
2023-07-04 10:59:47 +0700

    add getUserData function

--- lib/features/auth/presentation/cubit/auth_cubit.dart
+++ lib/features/auth/presentation/cubit/auth_cubit.dart
@@ -1,21 +1,32 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:siakad_sma_al_fusha/core/usecase/usecase.dart';
 import 'package:siakad_sma_al_fusha/env/env.dart';
+import 'package:siakad_sma_al_fusha/features/login/domain/usecase/get_user_usecase.dart';
 
 part 'auth_state.dart';
 
 class AuthCubit extends Cubit<AuthState> {
-  AuthCubit(this.preferences) : super(AuthInitial());
+  AuthCubit(this.preferences, this.useCase) : super(AuthInitial());
 
   final SharedPreferences preferences;
+  final GetUserUseCase useCase;
 
   void checkAuthenticationStatus() {
-    final status = preferences.containsKey(Env.token);
+    final status = preferences.containsKey(Env.user);
     if(status){
-      emit(AuthAuthenticated());
+      getUserData();
     } else {
       emit(AuthUnauthorized());
     }
+  }
+
+  void getUserData() async {
+    final result = await useCase(NoParams());
+    result!.fold(
+      (l) => emit(AuthUnauthorized()), 
+      (r) => emit(AuthAuthenticated(r.data[0].role))
+    );
   }
 }
