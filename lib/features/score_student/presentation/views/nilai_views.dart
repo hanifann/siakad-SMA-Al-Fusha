93629d4868a343e3b47c2ebf72d6ed4084b150cf
hanifann <hanifannurulhaq@outlook.com>
2023-07-27 16:23:05 +0700

    implement tehunAjran bloc

--- lib/features/score_student/presentation/views/nilai_views.dart
+++ lib/features/score_student/presentation/views/nilai_views.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shimmer/shimmer.dart';
+import 'package:siakad_sma_al_fusha/features/login/presentation/widgets/column_title_and_textfield_widget.dart';
+import 'package:siakad_sma_al_fusha/features/score_student/domain/entities/tahun_ajaran.dart';
 import 'package:siakad_sma_al_fusha/features/score_student/presentation/bloc/nilai_bloc.dart';
+import 'package:siakad_sma_al_fusha/features/score_student/presentation/bloc/tahun_ajaran_bloc.dart';
 import 'package:siakad_sma_al_fusha/features/score_student/presentation/widgets/container_nilai_widget.dart';
+import 'package:siakad_sma_al_fusha/features/score_student/presentation/widgets/custom_dropdown_btn_widget.dart';
 import 'package:siakad_sma_al_fusha/injection_container.dart';
 import 'package:siakad_sma_al_fusha/widgets/text_widget.dart';
 
@@ -11,8 +16,15 @@ class NilaiView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => sl<NilaiBloc>()..add(GetUserIdEvent()),
+    return MultiBlocProvider(
+        providers: [
+        BlocProvider(
+          create: (context) => sl<NilaiBloc>()..add(GetUserIdEvent()),
+        ),
+          BlocProvider(
+            create: (context) => sl<TahunAjaranBloc>()..add(GetTahunAjaranEvent()),
+          ),
+        ],
       child: const NilaiPage(),
     );
   }
@@ -27,63 +39,132 @@ class NilaiPage extends StatefulWidget {
 
 class _NilaiPageState extends State<NilaiPage> {
   bool isExpand = false;
+  String? idUsers;
+  String? tahunAjaran;
+  bool isFirst = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: BlocConsumer<NilaiBloc, NilaiState>(
-        listener: (context, state) {
-          if(state is UserDataLoaded){
-            context.read<NilaiBloc>().add(GetNilaiEvent(state.user.data[0].idUsers));
-          }
-        },
-        builder: (context, state) {
-          if(state is NilaiLoaded){
-            return ListView.separated(
-              padding: EdgeInsets.all(16.r),
-              itemBuilder: (context, index) {
-                return ContainerNilaiWidget(
-                  isExpand: isExpand, 
-                  nilai: state.nilai.data[index], 
-                  onIconTap: () {
-                    setState(() {
-                      isExpand = !isExpand;
-                    });
-                  },
-                );
-              }, 
-              separatorBuilder: (_, __) => SizedBox(height: 16.h), 
-              itemCount: state.nilai.data.length
-            );
-          } else if (state is NilaiFailed){
-            return Center(
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 16.w),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset(
-                      'assets/images/error.png',
-                      height: 256.r,
-                      width: 256.r,
-                    ),
-                    SizedBox(height: 16.h,),
-                    CustomTextWidget(
-                      text: state.error.message!,
-                      size: 14.sp,
-                      weight: FontWeight.w500,
-                    )
-                  ],
-                ),
-              ),
-            ); 
-          } else {
-            return const Center(
-              child: CircularProgressIndicator.adaptive(),
-            );
-          }
-        },
+      body: ListView(
+        padding: EdgeInsets.all(16.r),
+        children: [
+          tahunAjaranBlocBuilderWidget(),
+          SizedBox(height: 16.h,),
+          nilaiBlocConsumerWidget()
+        ],
       )
+    );
+  }
+
+  BlocBuilder<TahunAjaranBloc, TahunAjaranState> tahunAjaranBlocBuilderWidget() {
+    return BlocBuilder<TahunAjaranBloc, TahunAjaranState>(
+      builder: (context, state) {
+        if(state is TahunAjaranLoaded){
+          if(isFirst){
+            context.read<NilaiBloc>().add(
+              GetNilaiEvent(idUsers!, state.tahunAjaran.data.first.id)
+            );
+          }
+
+          isFirst = false;
+
+          return ColumnTitleAndTextFieldWidget(
+            textfield: CustomDropdownBtnWidget(
+              items: state.tahunAjaran.data.map<DropdownMenuItem<String>>((TahunAjaranData e) {
+                return DropdownMenuItem<String>(
+                  value: e.id,
+                  child: CustomTextWidget(
+                    text: e.nameThnAjaran,
+                    size: 14.sp,
+                    weight: FontWeight.w500,
+                  ),
+                );
+              }).toList(), 
+              onChange: (value) {
+                setState(() {
+                  tahunAjaran = value!;
+                });
+                context.read<NilaiBloc>().add(
+                  GetNilaiEvent(idUsers!, tahunAjaran!)
+                );
+              },
+              value: tahunAjaran ?? state.tahunAjaran.data.first.id,
+            ), 
+            title: 'Tahun Ajaran'
+          );
+        } else if (state is TahunajaranFailed){
+          return CustomTextWidget(
+            text: state.error.message!
+          );
+        } else {
+          return Shimmer.fromColors(
+            baseColor: Colors.grey.shade300,
+            highlightColor: Colors.grey.shade100,
+            child: Container(
+              width: 1.sw,
+              height: 50.h,
+              color: Colors.white,
+            ),
+          );
+        }
+      },
+    );
+  }
+
+  BlocConsumer<NilaiBloc, NilaiState> nilaiBlocConsumerWidget() {
+    return BlocConsumer<NilaiBloc, NilaiState>(
+      listener: (context, state) {
+        if(state is UserDataLoaded){
+          setState(() {
+            idUsers = state.user.data[0].idUsers;
+          });
+        }
+      },
+      builder: (context, state) {
+        if(state is NilaiLoaded){
+          return ListView.separated(
+            primary: false,
+            shrinkWrap: true,
+            itemBuilder: (context, index) {
+              return ContainerNilaiWidget(
+                isExpand: isExpand, 
+                nilai: state.nilai.data[index], 
+                onIconTap: () {
+                  setState(() {
+                    isExpand = !isExpand;
+                  });
+                },
+              );
+            }, 
+            separatorBuilder: (_, __) => SizedBox(height: 16.h), 
+            itemCount: state.nilai.data.length
+          );
+        } else if (state is NilaiFailed){
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/images/error.png',
+                  height: 256.r,
+                  width: 256.r,
+                ),
+                SizedBox(height: 16.h,),
+                CustomTextWidget(
+                  text: state.error.message!,
+                  size: 14.sp,
+                  weight: FontWeight.w500,
+                )
+              ],
+            ),
+          ); 
+        } else {
+          return const Center(
+            child: CircularProgressIndicator.adaptive(),
+          );
+        }
+      },
     );
   }
 }
